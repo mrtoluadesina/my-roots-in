08eb668fe9c6d8d4bc0ebcc8e3d8f9935a85dfe2
Tolu Adesina <tadesina90@gmail.com>
2019-11-27 12:36:58 +0100

    update on the App abstraction

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import Home from './src/views/Home';
 
 export default function App() {
+  useEffect(() => {
+
+  })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
