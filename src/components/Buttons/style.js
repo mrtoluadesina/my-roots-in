93112643102764e235c9b21ceb0c48408fc787cb
Tolu Adesina <tadesina90@gmail.com>
2019-11-27 17:59:16 +0100

    add styling for the button

---
+++ src/components/Buttons/style.js
@@ -0,0 +1,10 @@
+import { TouchableOpacity } from "react-native";
+import styled from "styled-components";
+
+export const Button = styled(TouchableOpacity)`
+  width: 80%;
+  align-items: center;
+  border-radius: 5px;
+`;
+
+
