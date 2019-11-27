2a0513f098e8110030eb32aad6ba78939c420e91
Tolu Adesina <tadesina90@gmail.com>
2019-11-27 18:55:04 +0100

    add styling for linear gradient button

--- src/components/Buttons/style.js
+++ src/components/Buttons/style.js
@@ -1,8 +1,15 @@
 import { TouchableOpacity } from "react-native";
 import styled from "styled-components";
+import { LinearGradient } from "expo-linear-gradient";
 
 export const Button = styled(TouchableOpacity)`
   width: 80%;
   align-items: center;
   border-radius: 5px;
 `;
+
+export const LinearGradientButton = styled(LinearGradient)`
+  width: 80%;
+  align-items: center;
+  border-radius: 5px;
+`
