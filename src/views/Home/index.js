37ba2e9c8fc29eaf2017f768c4983a2d1ec9cdf5
Tolu Adesina <tadesina90@gmail.com>
2019-11-27 14:45:53 +0100

    update Home with simple button and pass props to it

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import { SimpleButton } from '../../components/Buttons';
+
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Text>Landing page</Text>
       <Text>My Roots In</Text>
+      <SimpleButton class={styles.btn} title="Press Me" onPress={() => Alert.alert('Simple Button')} />
     </View>
   );
 }
@@ -19,4 +22,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn: {
+    backgroundColor: "#dddddd",
+    paddingVertical: 5,
+    paddingHorizontal: 10
+  }
 });
