f423ebe954b8dace667b877f304bde0c67d5df4f
Tolu Adesina <tadesina90@gmail.com>
2019-11-27 13:04:50 +0100

    Add the fonts using useEffect

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import Home from './src/views/Home';
+import AppRoutes from './routes';
 
 export default function App() {
+  const [isFontReady, setIsFontReady] = useState(false);
   useEffect(() => {
+    const loadFont = async () => {
+      await isFontReady.loadAsync({
+        'Helvetica': require('./assets/fonts/HelveticaNeue.ttf'),
+        'Helvetica-Light': require('./assets/fonts/HelveticaNeue-Light.ttf'),
+        'Helvetica-Bold': require('./assets/fonts/Helvetica-Neu-Bold.tff')
+      });
+      setIsFontReady(true);
+    };
+    loadFont();
+  }, [isFontReady]);
 
-  })
-  return (
-    <Home />
-  );
+  return isFontReady ? <AppRoutes /> : null;
 }
