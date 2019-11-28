ab6c0189fc16079ff57d9ae7dc17e31a641cb000
Tolu Adesina <tadesina90@gmail.com>
2019-11-28 21:19:23 +0100

    add keyboard avoiding view to the signup view

--- src/views/Signup/index.js
+++ src/views/Signup/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import CustomInput from "../../components/Input";
+import { KeyboardAvoidingView, StyleSheet } from "react-native";
 import {
   Container,
   Background,
@@ -32,64 +33,50 @@ export default function Signup(props) {
 
   return (
     <Container>
-      <Background>
-        <Greeting>welcome</Greeting>
-        <Description>
-          To verify your identity. please fill your personal information
-        </Description>
-        <Form>
-          <CustomInput
-            defaultValue={fields[details[0].type]}
-            textContentType={details[0].text}
-            onChangeText={handleChange(details[0].type)}
-            {...details[0]}
-            style={{ marginBottom: 26 }}
-          />
-          <CustomInput
-            defaultValue={fields[details[1].type]}
-            textContentType={details[1].text}
-            onChangeText={handleChange(details[1].type)}
-            {...details[1]}
-            style={{ marginBottom: 26 }}
-          />
-          <CustomInput
-            defaultValue={fields[details[2].type]}
-            textContentType={details[2].text}
-            onChangeText={handleChange(details[2].type)}
-            {...details[2]}
-            style={{ marginBottom: 26 }}
-          />
-          <CustomInput
-            defaultValue={fields[details[3].type]}
-            textContentType={details[3].text}
-            onChangeText={handleChange(details[3].type)}
-            {...details[3]}
-            style={{ marginBottom: 26 }}
-          />
-          <CustomInput
-            defaultValue={fields[details[4].type]}
-            textContentType={details[4].text}
-            onChangeText={handleChange(details[4].type)}
-            {...details[4]}
-            style={{ marginBottom: 50 }}
-          />
-        </Form>
+      <KeyboardAvoidingView
+        behavior="position"
+        contentContainerStyle={StyleSheet.wrapper}
+      >
+        <Background>
+          <Greeting>welcome</Greeting>
+          <Description>
+            To verify your identity. please fill your personal information
+          </Description>
+          <Form>
+            {details.map((value, index) => (
+              <CustomInput
+                key={index}
+                defaultValue={fields[value.type]}
+                textContentType={value.text}
+                onChangeText={handleChange(value.type)}
+                {...value}
+                style={{ marginBottom: index != details.length - 1 ? 26 : 50 }}
+              />
+            ))}
+          </Form>
 
-        <SimpleLinearGradientButton
-          title="Submit"
-          class={{
-            width: "100%"
-          }}
-          textStyle={{
-            color: colors.rootWhite,
-            fontSize: 14
-          }}
-          onPress={() => navigate("Verify")}
-        />
-        <Signin onPress={() => navigate("Login")}>
-          Have an account signin?
-        </Signin>
-      </Background>
+          <SimpleLinearGradientButton
+            title="Submit"
+            class={{
+              width: "100%"
+            }}
+            textStyle={{
+              color: colors.rootWhite,
+              fontSize: 14
+            }}
+            onPress={() => navigate("Verify")}
+          />
+          <Signin onPress={() => navigate("Login")}>
+            Have an account signin?
+          </Signin>
+        </Background>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1
+  }
+});
