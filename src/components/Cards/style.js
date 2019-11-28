b18e5072d374efde6560341bceccd6c36efa3bb6
Tolu Adesina <tadesina90@gmail.com>
2019-11-28 04:12:56 +0100

    add styling to the Card using styled components

---
+++ src/components/Cards/style.js
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import styled from "styled-components";
+
+export const Card = styled(View)`
+  background: #fff;
+	min-height: 100px;
+	min-width: 100px;
+  padding: 10px;
+	border-radius: 10px;
+  border: 1px solid rgba(0, 0, 0, 0.07);
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
+`;
