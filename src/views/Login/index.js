e1e15da8300c5c7bd0542ffe3f061cbdf14b5544
Tolu Adesina <tadesina90@gmail.com>
2019-11-28 07:26:59 +0100

    create the Login View

---
+++ src/views/Login/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+// import CustomInput from '../../components/Input';
+// import metadata from '../../constants/meta';
+
+function Login(props) {
+  const {navigate} = props.navigation;
+  return (
+    <View>
+      <Text>Login Page</Text>
+    </View>
+  );
+}
+
+export default Login;
