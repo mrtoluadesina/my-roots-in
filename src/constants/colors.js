35369af17c52ac3bedeaf1f5fbed2c7e632aea00
Tolu Adesina <tadesina90@gmail.com>
2019-11-28 05:15:58 +0100

    add color for box border and box shadow

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -2,5 +2,7 @@ export const colors = {
   rootGreen: "#28D892",
   rootWhite: "#ffffff",
   rootBlack: "#111111",
-  rootShadow: "#9DA3B4"
+  rootShadow: "#9DA3B4",
+  boxBorder: "rgba(0, 0, 0, 0.02)",
+  boxShadow: "rgba(157, 163, 180, 0.15)"
 }
