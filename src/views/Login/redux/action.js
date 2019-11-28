2aec61a6ead205af6ccc73e80812f14d458055ba
Tolu Adesina <tadesina90@gmail.com>
2019-11-28 12:29:04 +0100

    update the Login action for redux

--- src/views/Login/redux/action.js
+++ src/views/Login/redux/action.js
@@ -1,7 +1,26 @@
 import * as types from "./types";
+import axios from "axios";
+import { BASE_URL } from "../../../constants/api";
 
 export const loading = isLoading => ({ type: types.SET_LOADING, isLoading });
 
 export const errorMethod = message => ({ type: types.SET_ERROR, message });
 
 export const setUser = token => ({ type: types.SET_USER, token });
+
+export const login = payload => dispatch => {
+  dispatch(loading(true));
+  return axios
+    .post(BASE_URL + "", payload)
+    .then(res => {
+      dispatch(loading(false));
+
+      return res.data;
+    })
+    .catch(error => {
+      dispatch(loading(false));
+      const { message } = error;
+      dispatch(errorMethod(message));
+      return error;
+    });
+};
