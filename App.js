64f892725211be97a2b78a995cf95b7d3c694b83
Tolu Adesina <tadesina90@gmail.com>
2019-11-28 09:56:08 +0100

    wrapped AppRoutes with Provider in App.js

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import * as Fonts from 'expo-font';
-import AppRoutes from './routes';
+import React, { useState, useEffect } from "react";
+import { Provider } from "react-redux";
+import store from './src/redux/store';
+import * as Fonts from "expo-font";
+import AppRoutes from "./routes";
 
 export default function App() {
   const [isFontReady, setIsFontReady] = useState(false);
   useEffect(() => {
     const loadFont = async () => {
       await Fonts.loadAsync({
-        'Helvetica': require('./assets/fonts/HelveticaNeue.ttf'),
-        'Helvetica-Light': require('./assets/fonts/HelveticaNeue-Light.ttf'),
-        'Helvetica-Bold': require('./assets/fonts/Helvetica-Neu-Bold.ttf')
+        Helvetica: require("./assets/fonts/HelveticaNeue.ttf"),
+        "Helvetica-Light": require("./assets/fonts/HelveticaNeue-Light.ttf"),
+        "Helvetica-Bold": require("./assets/fonts/Helvetica-Neu-Bold.ttf")
       });
       setIsFontReady(true);
     };
     loadFont();
   }, [isFontReady]);
 
-  return isFontReady ? <AppRoutes /> : null;
+  return isFontReady ? (
+    <Provider store={store}>
+      <AppRoutes />
+    </Provider>
+  ) : null;
 }
