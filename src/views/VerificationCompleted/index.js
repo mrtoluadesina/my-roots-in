5c0c61c62e23273e045e76d51f81987fed6fe3f9
jagajastic <josephibrahi@gmail.com>
2019-11-28 19:37:26 +0100

    add image and content to verification page

--- src/views/VerificationCompleted/index.js
+++ src/views/VerificationCompleted/index.js
@@ -1,46 +1,71 @@
 import React from "react";
 import { View, Text, ScrollView, StyleSheet, Image } from "react-native";
+import { images } from "../../../assets/images";
+import { SimpleLinearGradientButton } from "../../components/Buttons";
 
 export default function VerificationCompleted() {
   return (
-    <ScrollView style={styles.main}>
-      <View style={styles.container}>
-        <View style={styles.imageContainer}>
-          <Image></Image>
-          <Text>Image for children playing</Text>
-        </View>
-        <View style={styles.article}>
-          <Text style={styles.heading}>Verification successful</Text>
-          <Text style={styles.paragraph}>
-            Your verification has been verified
-          </Text>
-          <Text style={styles.paragraph}>please login with your details</Text>
-        </View>
-        <View style={styles.footer}>
-          <Text>Got it!</Text>
-        </View>
+    <View style={styles.container}>
+      <Image source={images.verification} style={styles.imageContainer}></Image>
+      <View style={styles.section}>
+        <Text style={styles.heading}>Verification successful</Text>
+        <Text style={styles.paragraph}>
+          Your verification has been verified please login with your details
+        </Text>
+        <SimpleLinearGradientButton
+          title="Got it!"
+          textStyle={styles.textColor}
+          class={styles.fullWidth}
+        ></SimpleLinearGradientButton>
       </View>
-    </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  main: {
+  container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    padding: 50,
+    paddingTop: 0,
+    paddingBottom: 0,
+    marginVertical: "20%"
   },
-  container: {
-    borderStyle: "solid",
-    borderColor: "red",
-    backgroundColor: "grey",
-    width: "80%",
-    height: 500,
-    alignItems: "center",
-    justifyContent: "center"
+
+  fullWidth: {
+    marginTop: 35,
+    width: "100%"
   },
-  imageContainer: {},
-  article: {},
-  heading: {},
-  paragraph: {}
+
+  imageContainer: {
+    width: 150,
+    height: 150
+  },
+  textColor: {
+    color: "white",
+    fontSize: 14,
+    fontFamily: "Helvetica-Bold"
+  },
+  section: {
+    marginTop: 50,
+    width: "90%",
+    alignItems: "center"
+  },
+
+  heading: {
+    fontSize: 20,
+    lineHeight: 24,
+    letterSpacing: 1,
+    fontFamily: "Helvetica-Bold"
+  },
+  paragraph: {
+    fontSize: 15,
+    lineHeight: 18,
+    letterSpacing: 0.5,
+    fontFamily: "Helvetica",
+    color: "#313543",
+    width: 250,
+    textAlign: "center",
+    marginTop: 20
+  }
 });
