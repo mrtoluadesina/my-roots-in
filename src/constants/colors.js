eccb72fd29ead12f178278ccecfcccf85c62d4e0
Tolu Adesina <tadesina90@gmail.com>
2019-11-28 05:20:06 +0100

    add linearGradient from and to colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -4,5 +4,7 @@ export const colors = {
   rootBlack: "#111111",
   rootShadow: "#9DA3B4",
   boxBorder: "rgba(0, 0, 0, 0.02)",
-  boxShadow: "rgba(157, 163, 180, 0.15)"
+  boxShadow: "rgba(157, 163, 180, 0.15)",
+  linearGradientFrom: "#0AC4BA",
+  linearGradientTo: "#2BDA8E"
 }
