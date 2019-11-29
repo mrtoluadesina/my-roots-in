d248fa68dc462909582c4d3bd806e8aaf7f049a7
jagajastic <josephibrahi@gmail.com>
2019-11-29 18:44:58 +0100

    add simple card to view, and  add navigation to the button

--- src/views/VerificationCompleted/index.js
+++ src/views/VerificationCompleted/index.js
@@ -1,53 +1,57 @@
 import React from "react";
-import { View, Text, ScrollView, StyleSheet, Image } from "react-native";
+import { StyleSheet } from "react-native";
 import { images } from "../../../assets/images";
 import { SimpleLinearGradientButton } from "../../components/Buttons";
+import { SimpleCard } from "../../components/Cards/index";
+import { colors } from "../../constants/colors";
 import {
   Container,
   ImageContainer,
   Section,
   Heading,
-  Paragraph,
-  FullWidth,
-  TextColor
+  Paragraph
 } from "./styles";
 
-export default function VerificationCompleted() {
+export default function VerificationCompleted(props) {
+  const { navigate } = props.navigation;
   return (
     <Container>
-      <ImageContainer source={images.verification}></ImageContainer>
-      <Section>
-        <Heading>Verification successful</Heading>
-        <Paragraph>
-          Your verification has been verified please login with your details
-        </Paragraph>
-        <SimpleLinearGradientButton
-          title="Got it!"
-          textStyle={styles.textColor}
-          class={styles.fullWidth}
-        ></SimpleLinearGradientButton>
-      </Section>
+      <SimpleCard style={styles.shadow}>
+        <ImageContainer source={images.verification}></ImageContainer>
+        <Section>
+          <Heading>Verification successful</Heading>
+          <Paragraph>
+            Your verification has been verified please login with your details
+          </Paragraph>
+          <SimpleLinearGradientButton
+            title="Got it!"
+            textStyle={styles.textColor}
+            class={styles.fullWidth}
+            onPress={() => navigate("Login")}
+          ></SimpleLinearGradientButton>
+        </Section>
+      </SimpleCard>
     </Container>
-    // <View style={styles.container}>
-    //   <Image source={images.verification} style={styles.imageContainer}></Image>
-    //   <View style={styles.section}>
-    //     <Text style={styles.heading}>Verification successful</Text>
-    //     <Text style={styles.paragraph}>
-    //       Your verification has been verified please login with your details
-    //     </Text>
-    //     <SimpleLinearGradientButton
-    //       title="Got it!"
-    //       textStyle={styles.textColor}
-    //       class={styles.fullWidth}
-    //     ></SimpleLinearGradientButton>
-    //   </View>
-    // </View>
   );
 }
 
 const styles = StyleSheet.create({
- 
-
+  shadow: {
+    borderColor: colors.rootShadow,
+    shadowColor: colors.rootShadow,
+    width: "80%",
+    height: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    shadowOffset: {
+      width: 0,
+      height: 15
+    },
+    shadowOpacity: 0.4,
+    shadowRadius: 55,
+    elevation: 1,
+    borderRadius: 10
+  },
   fullWidth: {
     marginTop: 35,
     width: "100%"
@@ -56,5 +60,5 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 14,
     fontFamily: "Helvetica-Bold"
-  },
+  }
 });
