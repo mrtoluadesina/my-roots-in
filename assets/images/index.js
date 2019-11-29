830d7efcac3c85a5382df8b57e144448148a9ab4
josemak25 <amakirij@gmail.com>
2019-11-29 16:58:47 +0100

    add images for deault use

--- assets/images/index.js
+++ assets/images/index.js
@@ -1,6 +1,14 @@
+import AnniversaryImage from "./anniversary";
+import GiftImage from "./gift";
+import HolidayImage from "./holidays";
+import OtherImage from "./other";
+
 export const images = {
   loginImg: require("./login.png"),
   getStartedImg: require("./get-started-tree.png"),
   getDefaultAvater: require("./avatar.png"),
-  forgotPasswordImg: require('./forgot-password.png')
-}
+  HolidayImage,
+  GiftImage,
+  OtherImage,
+  AnniversaryImage
+};
