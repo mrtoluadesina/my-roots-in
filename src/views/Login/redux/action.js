0b43632c2a22fc20170c395b06421df5f92fbe6d
whitehox <oketegah@gmail.com>
2019-11-29 14:24:30 +0100

    formatting

--- src/views/Login/redux/action.js
+++ src/views/Login/redux/action.js
@@ -1,6 +1,6 @@
-import * as types from "./types";
-import axios from "axios";
-import { BASE_URL } from "../../../constants/api";
+import * as types from './types';
+import axios from 'axios';
+import { BASE_URL } from '../../../constants/api';
 
 export const loading = isLoading => ({ type: types.SET_LOADING, isLoading });
 
@@ -11,7 +11,7 @@ export const setUser = token => ({ type: types.SET_USER, token });
 export const login = payload => dispatch => {
   dispatch(loading(true));
   return axios
-    .post(BASE_URL + "", payload)
+    .post(BASE_URL + '', payload)
     .then(res => {
       dispatch(loading(false));
 
