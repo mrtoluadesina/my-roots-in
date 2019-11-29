938afebbc9e0f46114bc9389c6b53e69a69955bb
Tolu Adesina <tadesina90@gmail.com>
2019-11-29 16:37:27 +0100

    update the dashboard screen with the overview

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,17 +1,43 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, ImageBackground } from "react-native";
-import { colors } from "../../constants/colors";
-import { Container, ImageBg, Header, Greeting, Avatar } from "./styles";
+import { StyleSheet, View, Text } from "react-native";
+import { SimpleCard } from "../../components/Cards";
+import {
+  Container,
+  ImageBg,
+  Row,
+  Avatar,
+  OverviewImage,
+  DetailsView,
+  ImageView
+} from "./styles";
 import { images } from "../../../assets/images";
+import { colors } from "../../constants/colors";
 
 function Dashboard(props) {
   return (
     <Container contentContainerStyle={styles.dashboard}>
       <ImageBg source={images.dashboardBg}>
-        <Header>
-          <Greeting>Settings</Greeting>
+        <Row style={styles.header}>
           <Avatar source={images.getDefaultAvatar}></Avatar>
-        </Header>
+        </Row>
+        <Row>
+          <SimpleCard style={styles.overviewCard}>
+            <SimpleCard style={{ ...styles.details, ...styles.padSide }}>
+              <ImageView>
+                <OverviewImage source={images.getStartedImg} />
+              </ImageView>
+              <DetailsView>
+                <Text style={styles.spanTitle}>The 54 Countries</Text>
+                <View style={styles.details}>
+                  <Text style={styles.quantity}>5</Text>
+                  <Text style={styles.description}>
+                    You have 0 trees planted on the 54 countries of Africa
+                  </Text>
+                </View>
+              </DetailsView>
+            </SimpleCard>
+          </SimpleCard>
+        </Row>
       </ImageBg>
     </Container>
   );
@@ -21,8 +47,36 @@ export default Dashboard;
 
 const styles = StyleSheet.create({
   dashboard: {
-    alignItems: "center",
-    justifyContent: "center",
     height: "100%"
+  },
+  header: {
+    justifyContent: "flex-end"
+  },
+  overviewCard: {
+    width: "100%",
+    paddingHorizontal: 15,
+    paddingVertical: 10
+  },
+  details: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  padSide: {
+    paddingHorizontal: 15
+  },
+  spanTitle: {
+    color: colors.rootDashboardSpanTitle,
+    fontSize: 16,
+    letterSpacing: 1
+  },
+  quantity: {
+    fontSize: 40,
+    paddingRight: 10,
+    width:"15%"
+  },
+  description: {
+    flexWrap: 'wrap',
+    width: "85%"
   }
-}); 
+});
