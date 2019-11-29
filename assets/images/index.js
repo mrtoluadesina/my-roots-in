996114bec50e94976498750cb5227c492361f2dc
Tolu Adesina <tadesina90@gmail.com>
2019-11-29 12:47:33 +0100

    add dashboard image to the images object

--- assets/images/index.js
+++ assets/images/index.js
@@ -1,6 +1,7 @@
 export const images = {
   loginImg: require("./login.png"),
   getStartedImg: require("./get-started-tree.png"),
-  getDefaultAvater: require("./avatar.png"),
-  forgotPasswordImg: require('./forgot-password.png')
+  getDefaultAvatar: require("./avatar.png"),
+  forgotPasswordImg: require('./forgot-password.png'),
+  dashboardBg: require('./dashboard.png')
 }
