6c57ada924d01241ba1c5a54075918e556faf677
josemak25 <amakirij@gmail.com>
2019-11-29 16:59:57 +0100

    add component for use as occasion or gifting

---
+++ src/views/Occasions/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+import {
+  Container,
+  Background,
+  Greeting,
+  Header,
+  Avater,
+  Description,
+  Body,
+  Choices,
+  CardTitle
+} from "./styles";
+
+import { images } from "../../../assets/images";
+import { SimpleLinearGradientButton } from "../../components/Buttons";
+import { SimpleCard } from "../../components/Cards";
+import { colors } from "../../constants/colors";
+
+export default function OccasionTypes() {
+  return (
+    <Container>
+      <Background>
+        <Header>
+          <Greeting>Type of occasion</Greeting>
+          <Avater source={images.getDefaultAvater}></Avater>
+        </Header>
+        <Body>
+          <Description>Choose the type of occasion</Description>
+          <Choices>
+            <SimpleCard style={cardSizeStyle}>
+              <images.GiftImage />
+              <CardTitle>birthdays</CardTitle>
+            </SimpleCard>
+            <SimpleCard style={cardSizeStyle}>
+              <images.AnniversaryImage />
+              <CardTitle>anniversary</CardTitle>
+            </SimpleCard>
+            <SimpleCard style={cardSizeStyle}>
+              <images.HolidayImage />
+              <CardTitle>holidays</CardTitle>
+            </SimpleCard>
+            <SimpleCard style={cardSizeStyle}>
+              <images.OtherImage />
+              <CardTitle>others</CardTitle>
+            </SimpleCard>
+          </Choices>
+        </Body>
+        <SimpleLinearGradientButton
+          title="Next"
+          class={{
+            width: "90%"
+          }}
+          textStyle={{
+            color: colors.rootWhite,
+            fontSize: 14
+          }}
+          onPress={() => navigate("HowToPlant")}
+        />
+      </Background>
+    </Container>
+  );
+}
+
+const cardSizeStyle = {
+  height: 105,
+  width: 110,
+  marginBottom: 50,
+  position: "relative",
+  alignItems: "center",
+  justifyContent: "center"
+};
