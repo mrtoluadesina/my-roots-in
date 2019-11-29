49509a6d459df17c5eaf763dac8ef7d860515372
Tolu Adesina <tadesina90@gmail.com>
2019-11-29 07:25:12 +0100

    create the dashboard screen

---
+++ src/views/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { colors } from "../../constants/colors";
+import { Container } from "./styles";
+
+function Dashboard(props) {
+  return (
+    <Container>
+      <Text>Dashboard</Text>
+    </Container>
+  );
+}
+
+export default Dashboard;
