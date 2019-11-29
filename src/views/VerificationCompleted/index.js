0db3d15ad964c41b342134d184e45dd8eb2f6dbb
jagajastic <josephibrahi@gmail.com>
2019-11-29 16:37:52 +0100

    import and use the externalize styles

--- src/views/VerificationCompleted/index.js
+++ src/views/VerificationCompleted/index.js
@@ -2,70 +2,59 @@ import React from "react";
 import { View, Text, ScrollView, StyleSheet, Image } from "react-native";
 import { images } from "../../../assets/images";
 import { SimpleLinearGradientButton } from "../../components/Buttons";
+import {
+  Container,
+  ImageContainer,
+  Section,
+  Heading,
+  Paragraph,
+  FullWidth,
+  TextColor
+} from "./styles";
 
 export default function VerificationCompleted() {
   return (
-    <View style={styles.container}>
-      <Image source={images.verification} style={styles.imageContainer}></Image>
-      <View style={styles.section}>
-        <Text style={styles.heading}>Verification successful</Text>
-        <Text style={styles.paragraph}>
+    <Container>
+      <ImageContainer source={images.verification}></ImageContainer>
+      <Section>
+        <Heading>Verification successful</Heading>
+        <Paragraph>
           Your verification has been verified please login with your details
-        </Text>
+        </Paragraph>
         <SimpleLinearGradientButton
           title="Got it!"
           textStyle={styles.textColor}
           class={styles.fullWidth}
         ></SimpleLinearGradientButton>
-      </View>
-    </View>
+      </Section>
+    </Container>
+    // <View style={styles.container}>
+    //   <Image source={images.verification} style={styles.imageContainer}></Image>
+    //   <View style={styles.section}>
+    //     <Text style={styles.heading}>Verification successful</Text>
+    //     <Text style={styles.paragraph}>
+    //       Your verification has been verified please login with your details
+    //     </Text>
+    //     <SimpleLinearGradientButton
+    //       title="Got it!"
+    //       textStyle={styles.textColor}
+    //       class={styles.fullWidth}
+    //     ></SimpleLinearGradientButton>
+    //   </View>
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 50,
-    paddingTop: 0,
-    paddingBottom: 0,
-    marginVertical: "20%"
-  },
+ 
 
   fullWidth: {
     marginTop: 35,
     width: "100%"
-  },
-
-  imageContainer: {
-    width: 150,
-    height: 150
   },
   textColor: {
     color: "white",
     fontSize: 14,
     fontFamily: "Helvetica-Bold"
   },
-  section: {
-    marginTop: 50,
-    width: "90%",
-    alignItems: "center"
-  },
-
-  heading: {
-    fontSize: 20,
-    lineHeight: 24,
-    letterSpacing: 1,
-    fontFamily: "Helvetica-Bold"
-  },
-  paragraph: {
-    fontSize: 15,
-    lineHeight: 18,
-    letterSpacing: 0.5,
-    fontFamily: "Helvetica",
-    color: "#313543",
-    width: 250,
-    textAlign: "center",
-    marginTop: 20
-  }
 });
