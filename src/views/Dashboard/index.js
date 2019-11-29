80bded771dbecc05b54f44f94e8e2b96b4f53787
Tolu Adesina <tadesina90@gmail.com>
2019-11-29 12:47:49 +0100

    update the dashboard screen on the app

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, ImageBackground } from "react-native";
 import { colors } from "../../constants/colors";
-import { Container } from "./styles";
+import { Container, ImageBg, Header, Greeting, Avatar } from "./styles";
+import { images } from "../../../assets/images";
 
 function Dashboard(props) {
   return (
-    <Container>
-      <Text>Dashboard</Text>
+    <Container contentContainerStyle={styles.dashboard}>
+      <ImageBg source={images.dashboardBg}>
+        <Header>
+          <Greeting>Settings</Greeting>
+          <Avatar source={images.getDefaultAvatar}></Avatar>
+        </Header>
+      </ImageBg>
     </Container>
   );
 }
 
 export default Dashboard;
+
+const styles = StyleSheet.create({
+  dashboard: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%"
+  }
+}); 
