0ae738cb1daf49a0577fd511e173ad39ddb73bb2
Tolu Adesina <tadesina90@gmail.com>
2019-11-29 16:36:28 +0100

    update the card component for reuse

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -3,7 +3,7 @@ import { Alert } from "react-native";
 import { Card } from "./style";
 
 export const SimpleCard = props => (
-  <Card onPress={() => Alert.alert("This is how to press it")}>
+  <Card style={props.style} onPress={() => Alert.alert("This is how to press it")}>
     {props.children}
   </Card>
 );
