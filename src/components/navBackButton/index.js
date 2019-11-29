e06841c5d471d155ea90b1d6f787e8f763fa4197
whitehox <oketegah@gmail.com>
2019-11-29 06:41:26 +0100

    made progress bar take progress level props

--- src/components/navBackButton/index.js
+++ src/components/navBackButton/index.js
@@ -1,25 +1,26 @@
-import React from "react";
-import * as Progress from "react-native-progress";
+import React from 'react';
+import * as Progress from 'react-native-progress';
 
-import { Image, Platform } from "react-native";
-import { Container, ProgressContainer, TouchableOpacity } from "./styles";
-import { colors } from "../../constants/colors";
+import { Image, Platform } from 'react-native';
+import { Container, ProgressContainer, TouchableOpacity } from './styles';
+import { colors } from '../../constants/colors';
+import { setRecoveryProps } from 'expo/build/ErrorRecovery/ErrorRecovery';
 
-export default function NavBackButton({ onPress }) {
+export default function NavBackButton({ onPress, progressLevel }) {
   return (
     <Container Platform={Platform}>
       <TouchableOpacity onPress={onPress}>
-        <Image source={require("../../../assets/images/nav-back-button.png")} />
+        <Image source={require('../../../assets/images/nav-back-button.png')} />
       </TouchableOpacity>
       <ProgressContainer>
         <Progress.Bar
-          progress={0.5}
+          progress={progressLevel}
           color={colors.rootGreenDark}
           unfilledColor={colors.unfilledColor}
           borderWidth={0}
           height={2}
           width={350}
-          style={{ width: "90%" }}
+          style={{ width: '90%' }}
         />
       </ProgressContainer>
     </Container>
