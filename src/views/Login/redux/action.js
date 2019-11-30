54e85b87f4d6d2bd3a1b4a9184b779c38a933420
Tolu Adesina <tadesina90@gmail.com>
2019-11-30 18:57:11 +0100

    add token to Async Storage

--- src/views/Login/redux/action.js
+++ src/views/Login/redux/action.js
@@ -1,8 +1,7 @@
-import {AsyncStorage} from 'react-native';
-import * as types from './types';
-import axios from 'axios';
-import { BASE_URL } from '../../../constants/api';
-// import {ENDPOINT} from 'react-native-dotenv';
+import { AsyncStorage } from "react-native";
+import * as types from "./types";
+import axios from "axios";
+import { BASE_URL } from "../../../constants/api";
 
 export const loading = isLoading => ({ type: types.SET_LOADING, isLoading });
 
@@ -13,11 +12,12 @@ export const setUser = token => ({ type: types.SET_USER, token });
 export const login = payload => dispatch => {
   dispatch(loading(true));
   return axios
-    .post(BASE_URL + '/user/login', payload)
+    .post(BASE_URL + "/user/login", payload)
     .then(res => {
       dispatch(loading(false));
-      AsyncStorage.setItem('isLoggedIn', '1');
-      AsyncStorage.setItem('userData', JSON.stringify(res.data.payload));
+      AsyncStorage.setItem("isLoggedIn", "1");
+      AsyncStorage.setItem("userData", JSON.stringify(res.data.payload));
+      AsyncStorage.setItem("token", res.data.token);
       return res.data;
     })
     .catch(error => {
