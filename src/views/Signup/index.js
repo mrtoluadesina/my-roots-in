c5ec4c0ce009686bc6e1e7f678a9cda5866d4fa4
josemak25 <amakirij@gmail.com>
2019-12-05 16:23:11 +0100

    add scrollview to signup component

--- src/views/Signup/index.js
+++ src/views/Signup/index.js
@@ -54,25 +54,26 @@ function Signup(props) {
         behavior="position"
         contentContainerStyle={KeyboardWrapper}
       >
-        <Background>
-          <Greeting>welcome</Greeting>
-          <Description>
-            To verify your identity. please fill your personal information
-          </Description>
-          <Form>
-            {details.map((value, index) => (
-              <CustomInput
-                key={index}
-                defaultValue={fields[value.type]}
-                textContentType={value.text}
-                onChangeText={handleChange(value.type)}
-                {...value}
-                style={{
-                  marginBottom: index != details.length - 1 ? 25 : 20
-                }}
-              />
-            ))}
-          </Form>
+        <Container>
+          <Background>
+            <Greeting>welcome</Greeting>
+            <Description>
+              To verify your identity. please fill your personal information
+            </Description>
+            <Form>
+              {details.map((value, index) => (
+                <CustomInput
+                  key={index}
+                  defaultValue={fields[value.type]}
+                  textContentType={value.text}
+                  onChangeText={handleChange(value.type)}
+                  {...value}
+                  style={{
+                    marginBottom: index != details.length - 1 ? 25 : 20
+                  }}
+                />
+              ))}
+            </Form>
 
           <SimpleButton
             title="Submit"
@@ -85,6 +86,7 @@ function Signup(props) {
             Have an account signin?
           </Signin>
         </Background>
+        </Container>
       </KeyboardAvoidingView>
     </ImageBackground>
   );
