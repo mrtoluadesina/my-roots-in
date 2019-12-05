3422ef2b3bbec64e85202a032a8da44ac9f8e43a
jagajastic <josephibrahi@gmail.com>
2019-12-05 20:46:26 +0100

    update where to plant View

--- src/views/WhereToPlant/index.js
+++ src/views/WhereToPlant/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Picker, View } from "react-native";
 import {
   Container,
   Background,
@@ -11,53 +11,74 @@ import {
   Choices,
   CardTitle,
   CardImage,
-  cardSizeStyle
+  cardSizeStyle,
+  CountrySize,
+  ImageContainer,
+  selectOption
 } from "./styles";
 import { images } from "../../../assets/images";
-import { SimpleLinearGradientButton } from "../../components/Buttons";
+import { SimpleButton } from "../../components/Buttons";
 import { SimpleCard } from "../../components/Cards";
 import { colors } from "../../constants/colors";
-
+import RNPickerSelect from "react-native-picker-select";
 function WhereToPlant(props) {
   const { navigate } = props.navigation;
   return (
-    <Container>
-      <Background>
-        <Header>
-          <Greeting>Where to plant</Greeting>
-          <Avater source={images.getDefaultAvatar}></Avater>
-        </Header>
-        <Body>
-          <Description>Choose the location to plant your roots</Description>
-          <Choices>
-            <SimpleCard style={cardSizeStyle}>
-              <CardImage source={images.allCountriesImg} />
-              <CardTitle>54 Countries</CardTitle>
-            </SimpleCard>
-            <SimpleCard
-              style={{
-                ...cardSizeStyle,
-                backgroundColor: "#79C700"
-              }}
-            >
-              <CardImage source={images.greatGreenWallImg} />
-              <CardTitle>Great green wall</CardTitle>
-            </SimpleCard>
-          </Choices>
-        </Body>
-        <SimpleLinearGradientButton
-          title="Next"
-          class={{
-            width: "90%"
-          }}
-          textStyle={{
-            color: colors.rootWhite,
-            fontSize: 14
-          }}
-          onPress={() => navigate("PlantTree")}
-        />
-      </Background>
-    </Container>
+    <ImageContainer source={images.whereToPlantATreeBgImg}>
+      <Container>
+        <Background>
+          <Header>
+            <Avater source={images.getDefaultAvatar}></Avater>
+          </Header>
+          <Body>
+            <Greeting>Where to plant</Greeting>
+            <Description>Choose the location to plant your roots</Description>
+            <Choices>
+              <SimpleCard style={cardSizeStyle}>
+                <RNPickerSelect
+                  style={{}}
+                  onValueChange={value => console.log(value)}
+                  items={[
+                    { label: "Football", value: "football" },
+                    { label: "Baseball", value: "baseball" },
+                    { label: "Hockey", value: "hockey" }
+                  ]}
+                >
+                  <CardImage source={images.allCountriesImg} />
+                  <CardTitle>54 Countries</CardTitle>
+                </RNPickerSelect>
+              </SimpleCard>
+              <SimpleCard style={CountrySize}>
+                <CardTitle>Great green wall</CardTitle>
+                <CardTitle style={{ color: colors.rootGreenDark }}>
+                  Learn more
+                </CardTitle>
+              </SimpleCard>
+            </Choices>
+          </Body>
+          <SimpleButton
+            title="Next"
+            class={{
+              width: "90%",
+              backgroundColor: colors.rootGreenDark,
+              shadowColor: colors.rootGreenDark,
+              shadowOffset: {
+                width: 0,
+                height: 10
+              },
+              shadowOpacity: 0.51,
+              shadowRadius: 16.16,
+              elevation: 24
+            }}
+            textStyle={{
+              color: colors.rootWhite,
+              fontSize: 14
+            }}
+            onPress={() => navigate("PlantTree")}
+          />
+        </Background>
+      </Container>
+    </ImageContainer>
   );
 }
 
