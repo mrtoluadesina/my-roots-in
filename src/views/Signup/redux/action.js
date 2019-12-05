a5802afdbb57a2517603a7c398cfec3bae23b50c
Tolu Adesina <tadesina90@gmail.com>
2019-12-06 00:55:14 +0100

    update the redux action for sign up

--- src/views/Signup/redux/action.js
+++ src/views/Signup/redux/action.js
@@ -1,11 +1,12 @@
-import * as types from './types';
-import {AsyncStorage} from 'react-native';
-import axios from 'axios';
-import { BASE_URL } from '../../../constants/api';
+import * as types from "./types";
+import axios from "axios";
+import { BASE_URL } from "../../../constants/api";
 
-export const loading = isLoading => ({type: types.SET_LOADING, isLoading})
+export const loading = isLoading => ({ type: types.SET_LOADING, isLoading });
 
-export const errorMethod = message => ({type: types.SET_ERROR, message})
+export const errorMethod = message => ({ type: types.SET_ERROR, message });
+
+export const tokenMethod = token => ({ type: types.SET_TOKEN, token });
 
 export const register = payload => dispatch => {
   dispatch(loading(true));
@@ -13,13 +14,13 @@ export const register = payload => dispatch => {
     .post(BASE_URL + "/user", payload)
     .then(res => {
       dispatch(loading(false));
-      AsyncStorage.setItem("token", res.data.token);
-      return res.data
+      dispatch(tokenMethod(res.data.token));
+      return res.data;
     })
     .catch(error => {
       dispatch(loading(true));
-      const {message} = error;
+      const { message } = error;
       dispatch(errorMethod(message));
       return error;
-    })
-}
+    });
+};
