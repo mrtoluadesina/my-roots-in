864effc2f1c23e8aee042b148f535c84beaa3295
Tolu Adesina <tadesina90@gmail.com>
2019-12-06 00:55:39 +0100

    update the redux reducer for the signup feature

--- src/views/Signup/redux/reducer.js
+++ src/views/Signup/redux/reducer.js
@@ -1,8 +1,9 @@
-import { SET_LOADING, SET_ERROR } from "./types";
+import { SET_LOADING, SET_ERROR, SET_TOKEN } from "./types";
 
 const initialState = {
   isLoading: false,
-  error: ""
+  error: "",
+  token: ""
 };
 
 export default (state = initialState, action = {}) => {
@@ -16,6 +17,11 @@ export default (state = initialState, action = {}) => {
       return {
         ...state,
         error: action.message
+      };
+    case SET_TOKEN:
+      return {
+        ...state,
+        token: action.token
       };
     default:
       return state;
