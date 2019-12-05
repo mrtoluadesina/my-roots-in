35a44a3db1427c9dec8fba57de8dabc90a844b11
josemak25 <amakirij@gmail.com>
2019-12-05 13:22:48 +0100

    fix return value of header progress bar

--- src/components/navBackButton/index.js
+++ src/components/navBackButton/index.js
@@ -1,41 +1,31 @@
-import React from 'react';
-import * as Progress from 'react-native-progress';
+import React from "react";
+import * as Progress from "react-native-progress";
 
-import { Image, Platform } from 'react-native';
-import { Container, ProgressContainer, TouchableOpacity } from './styles';
-import { colors } from '../../constants/colors';
+import { Image, Platform } from "react-native";
+import { Container, ProgressContainer, TouchableOpacity } from "./styles";
+import { colors } from "../../constants/colors";
+import { images } from "../../../assets/images";
 
 export default function NavBackButton({
   onPress,
   showProgress,
-  progressLevel,
+  progressLevel
 }) {
   return (
     <Container Platform={Platform}>
       <TouchableOpacity onPress={onPress}>
-        <Image source={require('../../../assets/images/nav-back-button.png')} />
+        <Image source={images.backButtonImg} />
       </TouchableOpacity>
-      <ProgressContainer>
-        <Progress.Bar
-          progress={progressLevel}
-          color={colors.rootGreenDark}
-          unfilledColor={colors.unfilledColor}
-          borderWidth={0}
-          height={2}
-          width={350}
-          style={{ width: '90%' }}
-        />
-      </ProgressContainer>
       {showProgress ? (
         <ProgressContainer>
           <Progress.Bar
-            progress={0.5}
+            progress={progressLevel}
             color={colors.rootGreenDark}
             unfilledColor={colors.unfilledColor}
             borderWidth={0}
             height={2}
             width={350}
-            style={{ width: '90%' }}
+            style={{ width: "90%" }}
           />
         </ProgressContainer>
       ) : null}
