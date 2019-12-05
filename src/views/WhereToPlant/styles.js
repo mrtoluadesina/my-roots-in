01f58addd3eeca27855d7746449866face1be96f
jagajastic <josephibrahi@gmail.com>
2019-12-05 20:47:04 +0100

    add some style for where to plant view

--- src/views/WhereToPlant/styles.js
+++ src/views/WhereToPlant/styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { colors } from "../../constants/colors";
+import { black } from "ansi-colors";
 
 export const Container = styled.ScrollView`
   flex: 1;
@@ -11,7 +12,6 @@ export const Background = styled.View`
   padding: 0 50px;
   font-family: "Helvetica-Bold";
   font-weight: 800;
-  background-color: #ffffff;
   align-items: center;
   justify-content: center;
   color: ${colors.rootBlack};
@@ -21,13 +21,14 @@ export const Greeting = styled.Text`
   font-family: "Helvetica";
   font-size: 26px;
   line-height: 31px;
+  color: ${colors.rootGreenDark};
 `;
 
 export const Header = styled.View`
-  padding-top: 30px;
+  padding-top: 5px;
   flex-direction: row;
   width: 100%;
-  justify-content: space-between;
+  justify-content: flex-end;
   align-items: center;
 `;
 
@@ -49,12 +50,13 @@ export const Description = styled.Text`
   font-size: 20px;
   line-height: 24px;
   text-align: center;
+  padding-top: 40px;
 `;
 
 export const Choices = styled.View`
   flex-direction: row;
   flex-wrap: wrap;
-  margin-top: 50px;
+  margin-top: 70px;
   margin-bottom: 80px;
   width: 100%;
   justify-content: space-around;
@@ -68,27 +70,27 @@ export const CardTitle = styled.Text`
   color: #323643;
   opacity: 0.5;
   text-align: center;
-  position: absolute;
-  bottom: -20;
   width: 120%;
   font-family: "Helvetica-Bold";
 `;
 
 export const CardImage = styled.Image`
-  width: 100%;
-  height: 100%;
+  width: 90%;
+  height: 90%;
   border-radius: 2;
 `;
 
 export const cardSizeStyle = {
-  height: 105,
-  width: 110,
-  marginBottom: 50,
+  height: 150,
+  width: 130,
+  marginBottom: 20,
+  padding: 10,
   position: "relative",
   alignItems: "center",
   justifyContent: "center",
   borderWidth: 0.5,
-  borderRadius: 2,
+  borderRadius: 30,
+  backgroundColor: `white`,
   borderColor: `${colors.boxShadow}`,
   borderBottomWidth: 0,
   shadowColor: `${colors.boxShadow}`,
@@ -96,4 +98,26 @@ export const cardSizeStyle = {
   shadowOpacity: 0.5,
   shadowRadius: 2,
   elevation: 2
+};
+
+export const CountrySize = {
+  width: 110,
+  position: "relative",
+  top: 70
+};
+
+export const imgbg = {};
+
+export const ImageContainer = styled.ImageBackground`
+  width: 100%;
+  height: 100%;
+  flex: 1;
+`;
+
+export const selectOption = {
+  position: "absolute",
+  left: 0,
+  right: 0,
+  width: "100%",
+  borderColor: "black"
 };
