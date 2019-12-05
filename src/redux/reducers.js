d1c24284004c4db05aaef42d82200a3fe3b6f03e
Tolu Adesina <tadesina90@gmail.com>
2019-12-06 00:48:09 +0100

    update combined reducers with the verification reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import Login from '../views/Login/redux/reducer';
 import Register from '../views/Signup/redux/reducer';
+import Verify from '../views/Verify/redux/reducer';
 
 const rootReducer = combineReducers({
   Login,
-  Register
+  Register,
+  Verify
 })
 
 export default rootReducer;
