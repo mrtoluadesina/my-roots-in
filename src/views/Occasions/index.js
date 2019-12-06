8d14be4a42eeea948e79ea100cac4bbd8fa6780e
josemak25 <amakirij@gmail.com>
2019-12-06 01:29:03 +0100

    add changes to ocassion component for selecting options

--- src/views/Occasions/index.js
+++ src/views/Occasions/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { ImageBackground } from "react-native";
 
 import {
   Container,
@@ -10,55 +11,100 @@ import {
   Body,
   Choices,
   CardTitle,
-  cardSizeStyle
+  checkBoxStyle,
+  styles
 } from "./styles";
 
 import { images } from "../../../assets/images";
-import { SimpleLinearGradientButton } from "../../components/Buttons";
-import { SimpleCard } from "../../components/Cards";
+import { SimpleButton } from "../../components/Buttons";
+import { CheckBox } from "../../components/CheckBox";
 import { colors } from "../../constants/colors";
 
-export default function OccasionTypes() {
+export default function OccasionTypes(props) {
+  const { navigate } = props.navigation;
+
+  const [selected, setSelected] = useState({ isSelected: "" });
+
+  const handleChecked = key => {
+    setSelected({ ...selected, isSelected: key });
+  };
+
   return (
-    <Container>
-      <Background>
-        <Header>
-          <Greeting>Type of occasion</Greeting>
-          <Avater source={images.getDefaultAvater}></Avater>
-        </Header>
-        <Body>
-          <Description>Choose the type of occasion</Description>
-          <Choices>
-            <SimpleCard style={cardSizeStyle}>
-              <images.GiftImage />
-              <CardTitle>birthdays</CardTitle>
-            </SimpleCard>
-            <SimpleCard style={cardSizeStyle}>
-              <images.AnniversaryImage />
-              <CardTitle>anniversary</CardTitle>
-            </SimpleCard>
-            <SimpleCard style={cardSizeStyle}>
-              <images.HolidayImage />
-              <CardTitle>holidays</CardTitle>
-            </SimpleCard>
-            <SimpleCard style={cardSizeStyle}>
-              <images.OtherImage />
-              <CardTitle>others</CardTitle>
-            </SimpleCard>
-          </Choices>
-        </Body>
-        <SimpleLinearGradientButton
-          title="Next"
-          class={{
-            width: "90%"
-          }}
-          textStyle={{
-            color: colors.rootWhite,
-            fontSize: 14
-          }}
-          onPress={() => navigate("HowToPlant")}
-        />
-      </Background>
-    </Container>
+    <ImageBackground
+      source={images.howToPlantBgImg}
+      style={{
+        flex: 1,
+        width: "100%"
+      }}
+      imageStyle={{ resizeMode: "stretch" }}
+    >
+      <Container>
+        <Background>
+          <Header>
+            <Avater source={images.getDefaultAvatar}></Avater>
+          </Header>
+          <Body>
+            <Greeting>
+              Type of {props.title ? props.title : "occasion"}
+            </Greeting>
+            <Description>
+              Choose the type of {props.title ? props.title : "occasion"}
+            </Description>
+            <Choices>
+              <CheckBox
+                label="birthday"
+                value="isBirthday"
+                checked={selected.isSelected == "isBirthday" ? true : false}
+                shadowColor={colors.rootShadow}
+                handleChange={handleChecked}
+                styles={checkBoxStyle}
+              >
+                <images.GiftImage />
+                <CardTitle>birthdays</CardTitle>
+              </CheckBox>
+              <CheckBox
+                label="anniversary"
+                value="isAnniversary"
+                checked={selected.isSelected == "isAnniversary" ? true : false}
+                shadowColor={colors.rootShadow}
+                handleChange={handleChecked}
+                styles={checkBoxStyle}
+              >
+                <images.AnniversaryImg />
+                <CardTitle>anniversary</CardTitle>
+              </CheckBox>
+              <CheckBox
+                label="holidays"
+                value="isHolidays"
+                checked={selected.isSelected == "isHolidays" ? true : false}
+                shadowColor={colors.rootShadow}
+                handleChange={handleChecked}
+                styles={checkBoxStyle}
+              >
+                <images.HolidayImage />
+                <CardTitle>holidays</CardTitle>
+              </CheckBox>
+              <CheckBox
+                label="others"
+                value="isOthers"
+                checked={selected.isSelected == "isOthers" ? true : false}
+                shadowColor={colors.rootShadow}
+                handleChange={handleChecked}
+                styles={checkBoxStyle}
+              >
+                <images.OtherImage />
+                <CardTitle>others</CardTitle>
+              </CheckBox>
+            </Choices>
+          </Body>
+          <SimpleButton
+            title="Next"
+            class={styles.fullWidth}
+            textStyle={styles.textColor}
+            onPress={() => navigate("HowToPlant")}
+          />
+        </Background>
+      </Container>
+    </ImageBackground>
   );
 }
