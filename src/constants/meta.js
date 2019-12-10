11e13ebef82a2f432e1df451e564a1f0350be097
josemak25 <amakirij@gmail.com>
2019-12-10 12:13:53 +0100

    add all african countries on meta data

--- src/constants/meta.js
+++ src/constants/meta.js
@@ -65,5 +65,83 @@ export default metadata = {
     { value: "Gambia", label: "Gambia" },
     { value: "Tunisia", label: "Tunisia" },
     { value: "Ghana", label: "Ghana" }
+  ],
+
+  allAfricanCountries: [
+    { value: "Algeria", label: "Algeria" },
+    { value: "Angola", label: "Angola" },
+    { value: "Benin", label: "Benin" },
+    { value: "Botswana", label: "Botswana" },
+    {
+      value: "British Indian Ocean Territory",
+      label: "British Indian Ocean Territory"
+    },
+    { value: "Burkina Faso", label: "Burkina Faso" },
+    { value: "Burundi", label: "Burundi" },
+    { value: "Cameroon", label: "Cameroon" },
+    { value: "Cabo Verde", label: "Cabo Verde" },
+    { value: "Central African Republic", label: "Central African Republic" },
+    { value: "Chad", label: "Chad" },
+    { value: "Comoros", label: "Comoros" },
+    { value: "Congo", label: "Congo" },
+    {
+      value: "Congo (Democratic Republic of the)",
+      label: "Congo (Democratic Republic of the)"
+    },
+    { value: "Djibouti", label: "Djibouti" },
+    { value: "Egypt", label: "Egypt" },
+    { value: "Equatorial Guinea", label: "Equatorial Guinea" },
+    { value: "Eritrea", label: "Eritrea" },
+    { value: "Ethiopia", label: "Ethiopia" },
+    {
+      value: "French Southern Territories",
+      label: "French Southern Territories"
+    },
+    { value: "Gabon", label: "Gabon" },
+    { value: "Gambia", label: "Gambia" },
+    { value: "Ghana", label: "Ghana" },
+    { value: "Guinea", label: "Guinea" },
+    { value: "Guinea-Bissau", label: "Guinea-Bissau" },
+    { value: "Côte d'Ivoire", label: "Côte d'Ivoire" },
+    { value: "Kenya", label: "Kenya" },
+    { value: "Lesotho", label: "Lesotho" },
+    { value: "Liberia", label: "Liberia" },
+    { value: "Libya", label: "Libya" },
+    { value: "Madagascar", label: "Madagascar" },
+    { value: "Malawi", label: "Malawi" },
+    { value: "Mali", label: "Mali" },
+    { value: "Mauritania", label: "Mauritania" },
+    { value: "Mauritius", label: "Mauritius" },
+    { value: "Mayotte", label: "Mayotte" },
+    { value: "Morocco", label: "Morocco" },
+    { value: "Mozambique", label: "Mozambique" },
+    { value: "Namibia", label: "Namibia" },
+    { value: "Niger", label: "Niger" },
+    { value: "Nigeria", label: "Nigeria" },
+    { value: "Réunion", label: "Réunion" },
+    { value: "Rwanda", label: "Rwanda" },
+    {
+      value: "Saint Helena, Ascension and Tristan da Cunha",
+      label: "Saint Helena, Ascension and Tristan da Cunha"
+    },
+    { value: "Sao Tome and Principe", label: "Sao Tome and Principe" },
+    { value: "Senegal", label: "Senegal" },
+    { value: "Seychelles", label: "Seychelles" },
+    { value: "Sierra Leone", label: "Sierra Leone" },
+    { value: "Somalia", label: "Somalia" },
+    { value: "South Africa", label: "South Africa" },
+    { value: "South Sudan", label: "South Sudan" },
+    { value: "Sudan", label: "Sudan" },
+    { value: "Swaziland", label: "Swaziland" },
+    {
+      value: "Tanzania, United Republic of",
+      label: "Tanzania, United Republic of"
+    },
+    { value: "Togo", label: "Togo" },
+    { value: "Tunisia", label: "Tunisia" },
+    { value: "Uganda", label: "Uganda" },
+    { value: "Western Sahara", label: "Western Sahara" },
+    { value: "Zambia", label: "Zambia" },
+    { value: "Zimbabwe", label: "Zimbabwe" }
   ]
 };
