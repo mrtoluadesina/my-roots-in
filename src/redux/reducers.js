3e913ef9775f22095e7ad63e8f7fdcc3ee55163c
Tolu Adesina <tadesina90@gmail.com>
2019-12-11 04:43:53 +0100

    update the combine reducers with plantTree reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux';
 import Login from '../views/Login/redux/reducer';
 import Register from '../views/Signup/redux/reducer';
 import Verify from '../views/Verify/redux/reducer';
+import PlantTree from '../views/Dashboard/redux/reducer';
 
 const rootReducer = combineReducers({
   Login,
   Register,
-  Verify
+  Verify,
+  roots: PlantTree
 })
 
 export default rootReducer;
